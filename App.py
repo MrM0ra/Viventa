957b3635e8a330f24954195f69dc3a62377152d5
unknown <themonstermora@gmail.com>
2021-05-22 23:05:53 -0500

    trying to parse the variables obtained to make the prediction. Wild 'ValueError: could not convert string to float: '[1, 1, 1, 1, 1.0, 1.0, 1, 1]' appears'

--- App.py
+++ App.py
@@ -1,30 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, request, flash
-import sklearn
+from model import *
+from joblib import dump, load
+import sklearn as sk
+import numpy
 
-app = Flask(__name__)
-
-@app.route('/', methods=["POST", "GET"])
-def hello_world():
-	if request.method == "GET":
-		return render_template('index.html')
-	else:
-		#currentuser = request.args.get("currentuser")
-		viv_type = request.form["viv_type"]
-		rooms = request.form["rooms"]
-		baths = request.form["baths"]
-		lots = request.form["lots"]
-		priv_area = request.form["priv_area"]
-		cons_area = request.form["cons_area"]
-		estrato = request.form["estrato"]
-		floor = request.form["floor"]
-		arr = [viv_type, rooms, baths, lots, priv_area, cons_area, estrato, floor]
-		print(arr)
-		return redirect(url_for("prediction", args=arr, code=307))
-
-@app.route('/prediction/<args>')
-def prediction(args):
-	print(args)
-	return render_template("prediction.html", args=args)
 
 '''
 #orden: 
@@ -38,6 +17,36 @@ estrato
 pisos
 '''
 
+app = Flask(__name__)
+
+@app.route('/', methods=["POST", "GET"])
+def hello_world():
+	if request.method == "GET":
+		return render_template('index.html')
+	else:
+		#currentuser = request.args.get("currentuser")
+		viv_type = request.form["viv_type"]
+		if "Casa" in viv_type:
+			viv_type = 1
+		else:
+			viv_type = 0
+
+		rooms = (int(request.form["rooms"]))
+		baths = (int(request.form["baths"]))
+		lots = (int(request.form["lots"]))
+		priv_area = (float(request.form["priv_area"]))
+		cons_area = (float(request.form["cons_area"]))
+		estrato = (int(request.form["estrato"]))
+		floor = (int(request.form["floor"]))
+		arr = [viv_type, rooms, baths, lots, priv_area, cons_area, estrato, floor]
+		return redirect(url_for("prediction", args=arr, code=307))
+
+@app.route('/prediction/<args>')
+def prediction(args):
+	clf=load('modelo_entrenado.pkl')
+	a = numpy.asarray(args, dtype="float64", order=None, like=None)
+	prediccion=clf.predict(a)
+	return render_template("prediction.html", args=prediccion)
 
 if __name__ == '__main__':
 	app.run(debug=True)

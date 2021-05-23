6c34cf6de095681227e2bf85c2d341f94cf7f744
unknown <themonstermora@gmail.com>
2021-05-22 23:52:53 -0500

    Wild ValueError: Expected 2D array, got 1D array instead:

--- App.py
+++ App.py
@@ -2,11 +2,11 @@ from flask import Flask, redirect, url_for, render_template, request, flash
 from model import *
 from joblib import dump, load
 import sklearn as sk
-import numpy
+import numpy as np
 
 
 '''
-#orden: 
+orden: 
 tipo de vivienda
 habitaciones
 baños
@@ -15,22 +15,25 @@ area privada
 area construida
 estrato
 pisos
+
+, dtype=np.float64
 '''
+
+array = np.array([1.0])
 
 app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def hello_world():
+	global array
 	if request.method == "GET":
 		return render_template('index.html')
 	else:
-		#currentuser = request.args.get("currentuser")
 		viv_type = request.form["viv_type"]
 		if "Casa" in viv_type:
 			viv_type = 1
 		else:
 			viv_type = 0
-
 		rooms = (int(request.form["rooms"]))
 		baths = (int(request.form["baths"]))
 		lots = (int(request.form["lots"]))
@@ -38,14 +41,16 @@ def hello_world():
 		cons_area = (float(request.form["cons_area"]))
 		estrato = (int(request.form["estrato"]))
 		floor = (int(request.form["floor"]))
-		arr = [viv_type, rooms, baths, lots, priv_area, cons_area, estrato, floor]
-		return redirect(url_for("prediction", args=arr, code=307))
+		array = np.array([viv_type, rooms, baths, lots, priv_area, cons_area, estrato, floor])
+		return redirect(url_for("prediction"))
 
-@app.route('/prediction/<args>')
-def prediction(args):
+
+@app.route('/prediction/')
+def prediction():
+	global array
 	clf=load('modelo_entrenado.pkl')
-	a = numpy.asarray(args, dtype="float64", order=None, like=None)
-	prediccion=clf.predict(a)
+	array.reshape(1, -1)
+	prediccion=clf.predict(array)
 	return render_template("prediction.html", args=prediccion)
 
 if __name__ == '__main__':
